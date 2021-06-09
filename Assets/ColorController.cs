6021da2866606f7fa227a9676655b619e89d3cd4
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-10 00:56:06 +0900

    FixedJointに関するスクリプトを追加した

---
+++ Assets/ColorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour
+{
+    [SerializeField] GameObject m_cube = null;
+    Color m_defaultColor;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = m_cube.GetComponent<Renderer>();
+        m_defaultColor = renderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeRed()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    public void ChangeBlue()
+    {
+        renderer.material.color = Color.blue;
+    }
+
+    public void ChangeYellow()
+    {
+        renderer.material.color = Color.yellow;
+    }
+
+    public void ChangeDefault()
+    {
+        renderer.material.color = m_defaultColor;
+    }
+}
---
+++ Assets/SelectedController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedController : MonoBehaviour
+{
+    [SerializeField] Dropdown m_dropdown = null;
+    Vector3 m_mousePosition = Vector3.zero;
+    bool m_selectedFlag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //m_mousePosition = Input.mousePosition;
+        //m_mousePosition.z = transform.position.z;
+        if (m_selectedFlag)
+        {
+            m_dropdown.gameObject.SetActive(true);
+        }
+        else
+        {
+            m_dropdown.gameObject.SetActive(false);
+        }
+    }
+
+    public void Selected()
+    {
+        if (!m_dropdown)
+        {
+            Debug.Log("DropDownがアサインされていません！");
+            return;
+        }
+        if (!m_selectedFlag)
+        {
+            m_selectedFlag = true;
+        }
+        else
+        {
+            m_selectedFlag = false;
+        }
+    }
+
+    public void DisSelected()
+    {
+        if (!m_dropdown)
+        {
+            Debug.Log("DropDownがアサインされていません！");
+            return;
+        }
+        m_dropdown.gameObject.SetActive(false);
+    }
+
+
+}

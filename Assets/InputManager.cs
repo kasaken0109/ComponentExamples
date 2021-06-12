ded67a6e934b1a727c53f54ed7ef7a48dd971241
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-12 21:55:11 +0900

    InputFieldのスクリプトを追加した

---
+++ Assets/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] Text m_text = null;
+    InputField inputField = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TextChange()
+    {
+        m_text.text = "あなたの名前：" + inputField.text;
+    }
+
+    public void TextChanging()
+    {
+        m_text.text = "入力中...";
+    }
+}

8208aa6a6bd33efe7bff053fba90cc81bf7d017c
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-12 21:59:48 +0900

    コメントを追加した

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
+    /// <summary>入力されたテキストを反映するテキストボックス/// </summary>
     [SerializeField] Text m_text = null;
     InputField inputField = null;
     // Start is called before the first frame update
@@ -19,11 +20,17 @@ public class InputManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// 入力が完了された時に変更を反映するための関数
+    /// </summary>
     public void TextChange()
     {
         m_text.text = "あなたの名前：" + inputField.text;
     }
 
+    /// <summary>
+    /// 入力中にテキストの表示を変える関数
+    /// </summary>
     public void TextChanging()
     {
         m_text.text = "入力中...";

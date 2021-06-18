06b7fc2424b8c25b1f4ae077a0338662951b7110
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-18 22:16:52 +0900

    NavMeshControllerを追加した

---
+++ Assets/NavMeshController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+    [SerializeField] GameObject m_player;
+    [SerializeField] float m_stopDistance = 2f;
+    NavMeshAgent navMeshAgent;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_player)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position,m_player.transform.position) > m_stopDistance)
+        {
+            navMeshAgent.SetDestination(m_player.transform.position);
+        }
+        else
+        {
+            renderer.material.color = Color.red;
+        }
+    }
+}
